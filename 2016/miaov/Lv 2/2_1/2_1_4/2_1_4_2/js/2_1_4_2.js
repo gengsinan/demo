5206730c8535d4d4454da5a30b1648a1bdf95f20
gengsinan <gengsinan@126.com>
2016-10-07 10:16:11 +0800

    10.7

--- 2016/miaov/Lv 2/2_1/2_1_4/2_1_4_2/js/2_1_4_2.js
+++ 2016/miaov/Lv 2/2_1/2_1_4/2_1_4_2/js/2_1_4_2.js
@@ -5,8 +5,6 @@ window.onload = function ()
 
 	aInput[1].onclick = function () 
 	{
-		for ( var i=1; i<10; i++)
-		{
 			if ( aInput[0].value == '' )
 			{
 				aSpan[0].innerHTML = '请输入QQ号';
@@ -15,7 +13,7 @@ window.onload = function ()
 			{
 				aSpan[0].innerHTML = '请输入正确格式(数字格式)';
 				aSpan[0].style.color = 'red';
-			} else if ( aInput[0].value.charAt(0) == '0' )			//此句语法跟下面一句重复，可以省略;写此句主要是学会运用charAt属性;
+			} else if ( aInput[0].value.charAt(0) == '0' )		
 			{
 				aSpan[0].innerHTML = '请输入正确格式(前面不能有0)';
 				aSpan[0].style.color = 'red';
@@ -27,16 +25,15 @@ window.onload = function ()
 			{
 				aSpan[0].innerHTML = '请输入正确格式(范围在5-10位数之内)';
 				aSpan[0].style.color = 'red';
-			} else if ( aInput[0].value.charAt(0) != i ) 
+			} else if ( aInput[0].value.charAt(0) != 1 && aInput[0].value.charAt(0) != 2 && aInput[0].value.charAt(0) != 3 && aInput[0].value.charAt(0) != 4 && aInput[0].value.charAt(0) != 5 && aInput[0].value.charAt(0) != 6 && aInput[0].value.charAt(0) != 7 && aInput[0].value.charAt(0) != 8 && aInput[0].value.charAt(0) != 9 ) 			
 			{
-				aSpan[0].innerHTML = '请输入正确格式(开头输入数字)';		//上面的！你跟我重复了！而且我比你还严谨！！！此句需要for循环支持;
+				aSpan[0].innerHTML = '请输入正确格式(开头输入数字)';		
 				aSpan[0].style.color = 'red';
 			} else
 			{
 				aSpan[0].innerHTML = '输入成功';
 				aSpan[0].style.color = 'green';
 			}
-		}
 	}
 /*
 1、有没有输入<br />
@@ -44,5 +41,6 @@ window.onload = function ()
 3、不能有0在前面<br />
 4、不能是小数<br />
 5、输入的数字必须在5位以上、10位以内<br />
+6、开头必须是数字
 */
 };
