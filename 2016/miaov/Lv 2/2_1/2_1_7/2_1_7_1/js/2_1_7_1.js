3de67812ad5574e56dc41c04681553b18c45f365
gengsinan <gengsinan@126.com>
2016-10-16 09:22:44 +0800

    9:22

--- 2016/miaov/Lv 2/2_1/2_1_7/2_1_7_1/js/2_1_7_1.js
+++ 2016/miaov/Lv 2/2_1/2_1_7/2_1_7_1/js/2_1_7_1.js
@@ -1,4 +1,23 @@
 window.onload = function () 
 {
-	
-}
+	var oEva = getElementById('evaluation');
+	var aOl = getElementsByTagName('ol');
+	var oLi1 = getElementsByTagName('li')[0];
+	var 
+
+	var arr = [];
+	var json = {};
+	var oTurn = true;
+	var num = null;
+	var quantity = 0;
+
+	for (var i=0; i<.length; i++ )
+	{
+		fn[i](A,B,C,D);
+	}
+
+	function fn1 (a,b,c,d)
+	{
+
+	}
+};
